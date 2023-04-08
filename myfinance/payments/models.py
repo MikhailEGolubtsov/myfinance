da528f64497e5097db40554dc63bdc4df9a51e25
Михаил Голубцов <mike345@list.ru>
2023-04-08 23:31:05 +0300

    Currency, Person models add

--- myfinance/payments/models.py
+++ myfinance/payments/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Currency(models.Model):
+    name = models.TextField()
+    slug = models.TextField()
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Person(models.Model):
+    name = models.TextField()
+    slug = models.TextField()
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
