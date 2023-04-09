c73b86f214d552740e6560dd968429fd63941bf7
Михаил Голубцов <mike345@list.ru>
2023-04-09 23:34:43 +0300

    PaymentType model added

--- myfinance/payments/models.py
+++ myfinance/payments/models.py
@@ -7,6 +7,7 @@ User = get_user_model()
 class Currency(models.Model):
     name = models.TextField()
     slug = models.TextField()
+    creation_date = models.DateTimeField(auto_now_add=True)
     description = models.TextField()
 
     def __str__(self):
@@ -16,7 +17,40 @@ class Currency(models.Model):
 class Person(models.Model):
     name = models.TextField()
     slug = models.TextField()
+    creation_date = models.DateTimeField(auto_now_add=True)
     description = models.TextField()
 
     def __str__(self):
         return self.name
+
+
+class PaymentType(models.Model):
+    name = models.TextField()
+    slug = models.TextField()
+    code = models.TextField()
+    creation_date = models.DateTimeField(auto_now_add=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Payment(models.Model):
+    sum = models.DecimalField()
+    person = models.ForeignKey(
+        Person,
+        on_delete=models.CASCADE,
+        related_name='payments',
+        verbose_name='Платеж'
+    )
+    score = models.SmallIntegerField(
+        verbose_name='Оценка',
+        validators=(
+            MinValueValidator(1),
+            MaxValueValidator(10)
+        ),
+    )
+    pub_date = models.DateTimeField(
+        verbose_name='Дата публикации',
+        auto_now_add=True
+    )
