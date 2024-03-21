a3f1431c21415b68a1e0b536361bd3f212bca75a
Mikhail Golubtsov <mike345@list.ru>
2024-03-22 00:25:28 +0300

    urls added

---
+++ myfinance/payments/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'payments'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('payments/<int:payment_id>/', views.post_detail, name='payment_detail'),
+    path('create/', views.post_create, name='payment_create'),
+    path('payments/<int:payment_id>/edit/', views.post_edit, name='payment_edit'),
+]
