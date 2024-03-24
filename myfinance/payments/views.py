b828ec38200c20fc7da1a3abd266959c6305c071
Mikhail Golubtsov <mike345@list.ru>
2024-03-25 00:02:21 +0300

    index page

--- myfinance/payments/views.py
+++ myfinance/payments/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth import get_user_model
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.cache import cache_page
+from django.conf import settings
 
-# Create your views here.
+from .models import Payment #, Group, Follow
+# from .forms import PostForm, CommentForm
+from .utils import pagination
+
+
+User = get_user_model()
+
+
+@cache_page(settings.CASHE_TIME, key_prefix='index_page')
+def index(request):
+    payment_list = Payment.objects.all()
+    page_obj = pagination(payment_list, request)
+    context = {
+        'page_obj': page_obj,
+    }
+    return render(request, 'payments/index.html', context)
